7e811e552a40c2d6fe5dd0a69951ecf02fae7a7b
SABATIER Boris <sabatier.boris@gmail.com>
2015-11-30 20:12:09 +0100

    Add template test for V1 packet

---
+++ src/slp/packet/packetV1_test.go
@@ -0,0 +1,43 @@
+package packet
+
+import "testing"
+
+func TestV1SrvReq(t *testing.T) {
+	// TODO
+}
+
+func TestV1SrvRply(t *testing.T) {
+	// TODO
+}
+
+func TestV1SrvReg(t *testing.T) {
+	// TODO
+}
+
+func TestV1SrvDereg(t *testing.T) {
+	// TODO
+}
+
+func TestV1SrvAck(t *testing.T) {
+	// TODO
+}
+
+func TestV1AttrRqst(t *testing.T) {
+	// TODO
+}
+
+func TestV1AttrRply(t *testing.T) {
+	// TODO
+}
+
+func TestV1DAAdvert(t *testing.T) {
+	// TODO
+}
+
+func TestV1SrvTypeRqst(t *testing.T) {
+	// TODO
+}
+
+func TestV1SrvTypeRply(t *testing.T) {
+	// TODO
+}
