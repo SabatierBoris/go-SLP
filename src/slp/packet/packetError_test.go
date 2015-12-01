9d22993698ab5991a1184fbc6665bf38edb39e9c
Boris SABATIER <sabatier.boris@gmail.com>
2015-12-01 16:11:55 +0100

    Add test for unsupported version

--- src/slp/packet/packetError_test.go
+++ src/slp/packet/packetError_test.go
@@ -14,3 +14,13 @@ func TestVersionUnknow(t *testing.T) {
 		t.Errorf("Test failed, expected an error, got:  '%s'", p)
 	}
 }
+
+func TestVersionUnsupported(t *testing.T) {
+	data := []byte{0x02}
+	buf := bytes.NewReader(data)
+
+	p, err := GetPacket(buf)
+	if err == nil {
+		t.Errorf("Test failed, expected an error, got:  '%s'", p)
+	}
+}
