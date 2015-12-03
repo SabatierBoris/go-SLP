0edf6048aeeacf8db094d5e46211411545a8d0ce
Boris SABATIER <sabatier.boris@gmail.com>
2015-12-03 16:59:36 +0100

    Correct error generation

--- src/slp/packet/packet.go
+++ src/slp/packet/packet.go
@@ -20,23 +20,38 @@ var Encoding = binary.BigEndian
 type Packet struct {
 	Version PacketVersion
 	Header  Header
+	Data    SLPFunction
 }
 
 func GetPacket(data io.Reader) (p Packet, err error) {
 	var v PacketVersion
 
 	if err = binary.Read(data, Encoding, &v); err != nil {
-		err = fmt.Errorf("Error during parsing packet version : ", err)
+		err = fmt.Errorf("Error during parsing packet version : %s", err)
 		return
 	}
 	p.Version = v
 
 	var h Header
-	if h, err = GetHeader(v); err != nil {
-		err = fmt.Errorf("Error during getting header type : ", err)
+	if h, err = GetHeader(p.Version); err != nil {
+		err = fmt.Errorf("Error during getting header type : %s", err)
 		return
 	}
-	h.Read(data)
+	if err = h.Read(data); err != nil {
+		err = fmt.Errorf("Error during parsing header : %s", err)
+		return
+	}
 	p.Header = h
+
+	var f SLPFunction
+	if f, err = GetFunction(p.Version, p.Header.GetFunction()); err != nil {
+		err = fmt.Errorf("Error during getting function type : %s", err)
+		return
+	}
+	if err = f.Read(data); err != nil {
+		err = fmt.Errorf("Error during parsing function : %s", err)
+		return
+	}
+	p.Data = f
 	return
 }
