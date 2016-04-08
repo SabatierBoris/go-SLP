23be2f8fefca776ea74e9344339879192806d1e6
SABATIER Boris <sabatier.boris@gmail.com>
2016-04-08 21:09:16 +0200

    Refactoring test with error type checking

--- src/slp/packet/packetError_test.go
+++ src/slp/packet/packetError_test.go
@@ -15,6 +15,13 @@ func TestVersionToto(t *testing.T) {
 		{[]byte{0x02}, &VersionError{}},
 		{[]byte{}, &ReadError{}},
 		{[]byte{0x01}, &ReadError{}},
+		{[]byte{0x01, 0xFF, 0x00, 0x0D, 0x00, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00}, &FunctionError{}},
+		{[]byte{0x01, 0x05, 0x00, 0x0D, 0x00, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00}, &ReadError{}},
+		{[]byte{0x01, 0x01, 0x00, 0x0D, 0x00, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00}, &FunctionError{}},
+		{[]byte{0x01, 0x05, 0x00, 0x0E, 0x02, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}, &FlagError{}},
+		{[]byte{0x01, 0x05, 0x00, 0x0E, 0x00, 0x01, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}, &DialectError{}},
+		{[]byte{0x01, 0x05, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}, &LanguageError{}},
+		{[]byte{0x01, 0x05, 0x00, 0x0E, 0x00, 0x00, 'z', 'z', 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}, &LanguageError{}},
 	}
 
 	for _, infos := range datas {
@@ -27,75 +34,5 @@ func TestVersionToto(t *testing.T) {
 			t.Errorf("Test failed, expected an %s, got:  '%s'", reflect.TypeOf(infos.result), err)
 		}
 		t.Logf("Got the error %s", err)
-	}
-}
-
-func TestFunctionUnknow(t *testing.T) {
-	data := []byte{0x01, 0xFF, 0x00, 0x0D, 0x00, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
-	}
-}
-
-func TestErrorFunctionParsing(t *testing.T) {
-	data := []byte{0x01, 0x05, 0x00, 0x0D, 0x00, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
-	}
-}
-
-func TestErrorFunctionUnsupported(t *testing.T) {
-	data := []byte{0x01, 0x01, 0x00, 0x0D, 0x00, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
-	}
-}
-
-func TestErrorV1Flags(t *testing.T) {
-	data := []byte{0x01, 0x05, 0x00, 0x0E, 0x02, 0x00, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
-	}
-}
-
-func TestErrorV1Dialect(t *testing.T) {
-	data := []byte{0x01, 0x05, 0x00, 0x0E, 0x00, 0x01, 'e', 'n', 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
-	}
-}
-
-func TestErrorV1NoLanguageCode(t *testing.T) {
-	data := []byte{0x01, 0x05, 0x00, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
-	}
-}
-
-func TestErrorV1UnknownLanguageCode(t *testing.T) {
-	data := []byte{0x01, 0x05, 0x00, 0x0E, 0x00, 0x00, 'z', 'z', 0x00, 0x03, 0x00, 0x00, 0x00, 0x00}
-	buf := bytes.NewReader(data)
-
-	p, err := GetPacket(buf) //TODO Check if the err is the right one
-	if err == nil {
-		t.Errorf("Test failed, expected an error, got:  '%v'", p)
 	}
 }
