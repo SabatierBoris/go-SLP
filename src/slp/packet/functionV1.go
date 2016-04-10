e52da9d784672603eef3ec966f65b1135e643308
SABATIER Boris <sabatier.boris@gmail.com>
2016-04-10 09:17:23 +0200

    Add godoc in functionV1.go file

--- src/slp/packet/functionV1.go
+++ src/slp/packet/functionV1.go
@@ -5,22 +5,26 @@ import (
 	"io"
 )
 
+// ErrorCode is use for get the ErrorCode of an Ack packet
 type ErrorCode uint16
 
+// Values of supported Error Code.
 const (
-	V1Success              = iota
-	V1ProtocolParseError   = iota
-	V1InvalidRegistration  = iota
-	V1ScopeNotSupported    = iota
-	V1CharsetNotUnderstood = iota
-	V1AuthenticationAbsent = iota
-	V1AuthenticationFalied = iota
+	V1Success              ErrorCode = iota
+	V1ProtocolParseError   ErrorCode = iota
+	V1InvalidRegistration  ErrorCode = iota
+	V1ScopeNotSupported    ErrorCode = iota
+	V1CharsetNotUnderstood ErrorCode = iota
+	V1AuthenticationAbsent ErrorCode = iota
+	V1AuthenticationFalied ErrorCode = iota
 )
 
+// SrvAckV1 is the structure of an SLP Ack packet for V1
 type SrvAckV1 struct {
 	ErrorCode ErrorCode
 }
 
+// Read parse the data for extract packet information
 func (f *SrvAckV1) Read(data io.Reader) (err error) {
 	if err = binary.Read(data, Encoding, f); err != nil {
 		err = &ReadError{}
@@ -29,6 +33,7 @@ func (f *SrvAckV1) Read(data io.Reader) (err error) {
 	return
 }
 
+// SrvAckV1Constructor is the constructor for SrvAckV1 packet
 func SrvAckV1Constructor() SLPFunction {
 	return &SrvAckV1{}
 }
